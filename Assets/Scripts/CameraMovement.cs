c77b45efabef329f426c8867f4bd2868231765ad
Ákos Balázs <profakos@gmail.com>
2020-10-24 13:02:56 +0200

    Initial commit

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+	
+	private readonly float speed = 20;
+	
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		if (Camera.current == null)
+			return;
+
+		float xAxisValue = Input.GetAxis("Horizontal");
+		float yAxisValue = Input.GetAxis("Vertical");
+
+		Vector2 cameraPosition = Camera.current.transform.position;
+		
+		var translation = speed * Time.deltaTime;
+
+		Camera.current.transform.Translate(new Vector2(xAxisValue * translation, yAxisValue * translation));
+
+	}
+}
---
+++ Assets/Scripts/Trainparts/Train.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Train : MonoBehaviour
+{
+	
+	[SerializeField]
+	// the lower the index, the righter most the car is, 0 index is the engine
+	private List<TrainCar> trainCars = new List<TrainCar>();
+
+	void Awake()
+	{
+		trainCars.AddRange(this.gameObject.transform.GetComponentsInChildren<TrainCar>());
+		trainCars.Sort((x, y) =>x.CarId.CompareTo(y.CarId));
+	}
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
